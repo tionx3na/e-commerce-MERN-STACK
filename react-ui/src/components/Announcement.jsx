d32394238424677ed4bfff7349583c68f7172020
xena <1runx3na2@gmail.com>
2021-12-15 22:53:52 +0530

    Navbar --completed

---
+++ react-ui/src/components/Announcement.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height:30px;
+    background-color: teal;
+    color:white;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    font-size:14px;
+`
+
+function Announcement() {
+    return (
+        <Container>
+            Super Deal for all the products 50% off!
+        </Container>
+    )
+}
+
+export default Announcement
