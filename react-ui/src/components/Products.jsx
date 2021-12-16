13f7270765ae5775b41cdbc250ffe8b9722757e0
xena <1runx3na2@gmail.com>
2021-12-16 20:48:48 +0530

    Product list --complete

---
+++ react-ui/src/components/Products.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+import { popularProducts } from '../data'
+import ProductItems from './ProductItems'
+
+const Container = styled.div`
+    padding:20px;
+    display:flex;
+    flex-wrap: wrap;
+    justify-content:space-between;
+`
+const Products = () => {
+    return (
+        <Container>
+            {popularProducts.map(item=>(
+                <ProductItems item={item} key={item.id}/>
+            ))}
+
+        </Container>
+    )
+}
+
+export default Products
--- react-ui/src/data.js
+++ react-ui/src/data.js
@@ -1,6 +1,13 @@
 import image1 from './assets/images/1.png'
 import image2 from './assets/images/2.png'
 import image3 from './assets/images/3.png'
+import a1 from './assets/images/a1.png'
+import a2 from './assets/images/a2.png'
+import a3 from './assets/images/a3.png'
+import a4 from './assets/images/a4.png'
+import a5 from './assets/images/a5.png'
+import a6 from './assets/images/a6.png'
+import a7 from './assets/images/a7.png'
 
 export const sliderItems = [
     {
@@ -41,5 +48,40 @@ export const categories = [
         id:3,
         img: "https://blog.venuelook.com/wp-content/uploads/2020/06/India-640x800.jpeg",
         title: "Wedding Dress",
+    },
+]
+
+export const popularProducts = [
+    {
+        id:1,
+        img:"https://i.pinimg.com/originals/62/98/b0/6298b026a65cf80bcf9dce061e9b79c9.png",
+    },
+    {
+        id:2,
+        img:a1,
+    },
+    {
+        id:3,
+        img:a2,
+    },
+    {
+        id:4,
+        img:a3,
+    },
+    {
+        id:5,
+        img:a4,
+    },
+    {
+        id:6,
+        img:a5,
+    },
+    {
+        id:7,
+        img:a6,
+    },
+    {
+        id:8,
+        img:a7,
     },
 ]
