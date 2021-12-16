b32d1e0cfc91181743220e9c82df2195a168d801
xena <1runx3na2@gmail.com>
2021-12-16 12:56:10 +0530

    Slider complete

---
+++ react-ui/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://wallpapercave.com/wp/wp8820372.jpg",
+        title: "Summer Kurti",
+        desc: "Best Kurti designs wih comfortbality and sleekness in mind!",
+        bg: "white",
+    },
+    {
+        id: 2,
+        img: "https://wallpapercave.com/wp/wp8820372.jpg",
+        title: "Summer Kurti",
+        desc: "Best Kurti designs wih comfortbality and sleekness in mind!",
+        bg: "white",
+    },
+    {
+        id: 3,
+        img: "https://wallpapercave.com/wp/wp8820372.jpg",
+        title: "Summer Kurti",
+        desc: "Best Kurti designs wih comfortbality and sleekness in mind!",
+        bg: "white",
+    }
+]
