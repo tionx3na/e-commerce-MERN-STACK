472d94bd8256953530304d23a50f9c36466d092e
xena <1runx3na2@gmail.com>
2021-12-22 22:54:37 +0530

    Login and Register page --complete

---
+++ react-ui/src/pages/Register.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import styled from "styled-components";
+import img1 from "../assets/images/register.jpg";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: 
+    linear-gradient(rgba(255,255,255,0.5),rgba(255,255,255,0.5)),
+    url(${(props) => props.background}) center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width:40%;
+  padding:20px;
+  background-color: white;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Input = styled.input`
+  flex:1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
+
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+`;
+
+const Register = () => {
+  return (
+    <Container background={img1}>
+      <Wrapper>
+        <Title>Create an account</Title>
+        <Form>
+          <Input placeholder="name" />
+          <Input placeholder="last name" />
+          <Input placeholder="user name" />
+          <Input placeholder="email" />
+          <Input placeholder="password" />
+          <Input placeholder="confirm password" />
+          <Agreement>
+            By creating an account, I consent to the processing of my personal
+            data in accordance with the <b>PRIVACY POLICY</b>
+          </Agreement>
+          <Button>CREATE</Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Register;
